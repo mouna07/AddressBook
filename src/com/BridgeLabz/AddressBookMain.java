01dc3c6995cd53241ac885384562006f8af79457
mouna07 <mounamc261@gmail.com>
2021-02-17 19:53:51 +0530

    ADDED: code for count person by city or state

--- src/com/BridgeLabz/AddressBookMain.java
+++ src/com/BridgeLabz/AddressBookMain.java
@@ -102,6 +102,42 @@ class AddressBookMain {
     }
 
 
+    public void CountByState(String state) {
+        int count = 0;
+        for(Map.Entry<String, AddressBook> entry: addressBookListMap.entrySet()){
+            for(int i=0;i<(entry.getValue()).contactList.size();i++)
+            {
+                ContactDetails contact= entry.getValue().contactList.get(i);
+
+                if(state.equals(contact.getState()))
+                {
+                    count++;
+                }
+
+            }
+        }
+        System.out.println("Total Person Count in state "+state+": "+count);
+    }
+    public void CountByCity(String city) {
+        int countPersonInCity=0;
+        for(Map.Entry<String, AddressBook> entry: addressBookListMap.entrySet())
+        {
+            for(int i=0;i<(entry.getValue()).contactList.size();i++)
+            {
+                ContactDetails d= (ContactDetails) entry.getValue().contactList.get(i);
+
+                if(city.equals(d.getCity()))
+                {
+                    countPersonInCity++;
+                }
+
+            }
+        }
+        System.out.println("Total number of people in this city "+city+": "+countPersonInCity);
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Address Book Management System using Java Stream");
         AddressBookMain addressBookMain = new AddressBookMain();
@@ -113,7 +149,9 @@ class AddressBookMain {
             System.out.println("4.Search Contact from a State");
             System.out.println("5.View contact By State Using State and Person HashMap");
             System.out.println("6.View Contact by city Using City and Person HashMap");
-            System.out.println("7.Exit");
+            System.out.println("7.Count Contact By State");
+            System.out.println("8.Count Contact By City");
+            System.out.println("9.Exit");
 
             System.out.println("Enter choice: ");
             int option = sc.nextInt();
@@ -162,6 +200,18 @@ class AddressBookMain {
                     break;
 
                 case 7:
+                    System.out.println("Enter Name of State: ");
+                    String stateName2 = sc.next();
+                    addressBookMain.CountByState(stateName2);
+                    break;
+
+                case 8:
+                    System.out.println("Enter Name of City: ");
+                    String cityName2 = sc.next();
+                    addressBookMain.CountByCity(cityName2);
+                    break;
+
+                case 9:
                     flag = false;
                     break;
             }
